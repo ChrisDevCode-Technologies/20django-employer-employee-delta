0a3f4ab74bd98f0093c6536da054f3e06cf95843
yusra1234567 <yusramuktar959@gmail.com>
2025-07-24 18:09:02 +0300

    refactor: switch to python-decouple for environment variable management in settings

--- core/settings.py
+++ core/settings.py
@@ -11,29 +11,23 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from pathlib import Path
-import environ
+from decouple import config
 
-# Load environment variables from .env file
-env = environ.Env(
-    # Set casting, default value
-    DEBUG = (bool, False)
-)
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(BASE_DIR / '.env')
-
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production  
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h^j&42dbs%9-ihtq&wis)m)*2_u@!77#09rqbbtp-_kwq2gba1'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-h^j&42dbs%9-ihtq&wis)m)*2_u@!77#09rqbbtp-_kwq2gba1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
